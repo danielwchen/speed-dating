f94b53918ddf499be366758d487b1c2c4f3bd2d4
Daniel Chen <danielchenphhs@gmail.com>
2017-07-13 15:21:51 -0400

    working version

--- js/radarChart2.js
+++ js/radarChart2.js
@@ -1,13 +1,3 @@
-//Practically all this code comes from https://github.com/alangrafu/radar-chart-d3
-//I only made some additions and aesthetic adjustments to make the chart look better
-//(of course, that is only my point of view)
-//Such as a better placement of the titles at each line end,
-//adding numbers that reflect what each circular level stands for
-//Not placing the last level and slight differences in color
-//
-//For a bit of extra information check the blog about it:
-//http://nbremer.blogspot.nl/2013/09/making-d3-radar-chart-look-bit-better.html
-
 var RadarChart = {
     draw: function(id, d, options){
         var cfg = {
